9cae1544c8e9d525bb0536aacf174d472c754173
seol <neoburi97@gmail.com>
2017-09-08 03:56:59 +0900

    My Django Girls app, first commit

---
+++ my_project/my_app_01/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyApp01Config(AppConfig):
+    name = 'my_app_01'
---
+++ my_project/my_app_02/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyApp02Config(AppConfig):
+    name = 'my_app_02'
